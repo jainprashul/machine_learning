b65007ecd53e58336f5450803139c0a22093c8e4
Prashul Jain <jainprashul@gmail.com>
2018-12-29 12:59:58 +0530

    regression training and testing

--- regression.py
+++ regression.py
@@ -1,6 +1,8 @@
 import pandas as pd
-import quandl 
-import math
+import numpy as np
+import quandl , math
+from sklearn import preprocessing, svm, model_selection
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -18,9 +20,28 @@ forecast_col = 'Adj. Close'
 # if data has nill value replace it by 9999
 df.fillna(-99999, inplace=True)
 
+# create predicted value as label
 forecast_out = int(math.ceil(0.01*len(df)))
+print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace = True)
 
-print(df.head())
+# defining datasets
+x = np.array(df.drop(['label'],1))
+y = np.array(df['label'])
+X = preprocessing.scale(x) 
+#x = x[: -forecast_out+ 1 ]
+#df.dropna(inplace=True)
+y = np.array(df['label'])
+
+X_train , X_test , y_train , y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+#classifier
+clf = LinearRegression(n_jobs=-1) 
+# train
+clf.fit(X_train, y_train)
+# test
+accuracy = clf.score(X_test, y_test)
+
+print(accuracy)
