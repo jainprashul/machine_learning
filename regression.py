378faaec7ed009dd76cb2c45f779418d78de75d4
Prashul Jain <jainprashul@gmail.com>
2018-12-31 19:39:32 +0530

    forecasting and predicting via plotin

--- regression.py
+++ regression.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import numpy as np
-import quandl , math
+import quandl , math , datetime
 from sklearn import preprocessing, svm, model_selection
 from sklearn.linear_model import LinearRegression
+from matplotlib import style
+import matplotlib.pyplot as plt
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -28,12 +32,15 @@ df['label'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace = True)
 
 # defining datasets
-x = np.array(df.drop(['label'],1))
+X = np.array(df.drop(['label'],1))
+X = preprocessing.scale(X) 
+x = X[: -forecast_out ]
+X_lately = X[-forecast_out:]
+
+df.dropna(inplace=True)
 y = np.array(df['label'])
-X = preprocessing.scale(x) 
-#x = x[: -forecast_out+ 1 ]
-#df.dropna(inplace=True)
 y = np.array(df['label'])
+
 
 X_train , X_test , y_train , y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
@@ -44,4 +51,32 @@ clf.fit(X_train, y_train)
 # test
 accuracy = clf.score(X_test, y_test)
 
-print(accuracy)
+# for k in ['linear','poly','rbf','sigmoid']:
+#     clf = svm.SVR(kernel=k)
+#     clf.fit(X_train, y_train)
+#     confidence = clf.score(X_test, y_test)
+#     print(k,confidence)
+
+forecast_set = clf.predict(X_lately)
+print(forecast_set, accuracy, forecast_out)
+
+
+# ploting the graph
+df['Forecast'] = np.nan
+# this is for date setting
+last_date = df.iloc[-1].name 
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
