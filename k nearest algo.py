9f5c155f9de75b66b35164355a4a3c51c862233c
Prashul Jain <jainprashul@gmail.com>
2019-01-02 01:07:34 +0530

    added pdf

--- k nearest algo.py
+++ k nearest algo.py
@@ -1,15 +1,9 @@
 from math import sqrt
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import style
 import warnings
 from collections import Counter
-style.use('fivethirtyeight')
-
-dataset = {'k': [[1,2],[2,3],[3,1]],
-           'r': [[6,5],[7,7],[8,6]]
-           }
-new_features = [5,7]
+import pandas as pd
+import random
 
 
 def k_nearest_neighbors(data, predict, k=3):
@@ -29,9 +23,45 @@ def k_nearest_neighbors(data, predict, k=3):
     return vote_result
 
 
-result = k_nearest_neighbors(dataset, new_features, k=3)
-print(result)
+# result = k_nearest_neighbors(dataset, new_features, k=3)
+# print(result)
 
-[[plt.scatter(ii[0],ii[1],s=100, color=i) for ii in dataset[i] ] for i in dataset]
-plt.scatter(new_features[0], new_features[1], color=result)
-plt.show()
+# [[plt.scatter(ii[0],ii[1],s=100, color=i) for ii in dataset[i] ] for i in dataset]
+# plt.scatter(new_features[0], new_features[1], color=result)
+# plt.show()
+
+# read the data
+df = pd.read_csv('breast-cancer-wisconsin.data.txt')
+df.replace('?', -99999 , inplace=True)
+df.drop(['id'],1,inplace=True)
+# set vlues as float
+full_data = df.astype(float).values.tolist()
+random.shuffle(full_data)
+
+# creating data sets 
+
+test_size = 0.2
+train_set = {2:[], 4:[]}
+test_set = {2:[], 4:[]}
+train_data = full_data[:-int(test_size*len(full_data))]
+test_data = full_data[-int(test_size*len(full_data)):]
+
+for i in train_data:
+    train_set[i[-1]].append(i[:-1])
+
+for i in test_data:
+    test_set[i[-1]].append(i[:-1])
+
+print(train_set, test_set)
+
+correct = 0
+total = 0
+
+for group in test_set:
+    for data in test_set[group]:
+        vote = k_nearest_neighbors(train_set, data, k=5)
+        if group == vote:
+            correct += 1
+        total += 1
+
+print("accuracy: ", correct/total)
