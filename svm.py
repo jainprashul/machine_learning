9a02edd6b36795f4d907134a838666de1dcfaa2f
Prashul Jain <jainprashul@gmail.com>
2019-01-02 12:10:39 +0530

    svm by sclearn

--- svm.py
+++ svm.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn import preprocessing, neighbors, model_selection
+from sklearn import preprocessing, neighbors, model_selection, svm
 import pandas as pd
 
 df = pd.read_csv('breast-cancer-wisconsin.data.txt')
@@ -11,14 +11,14 @@ y = np.array(df['class'])
 
 X_train , X_test , y_train, y_test = model_selection.train_test_split(X,y, test_size=0.2)
 
-clf = neighbors.KNeighborsClassifier()
+clf = svm.SVC()
 clf.fit(X_train,y_train)
 
 accuracy = clf.score(X_test, y_test)
 print(accuracy)
 
-example_measures = np.array([4,2,1,1,1,2,3,2,1])
-example_measures = example_measures.reshape(len(example_measures),-1)
+# example_measures = np.array([4,2,1,1,1,2,3,2,1])
+# example_measures = example_measures.reshape(len(example_measures),-1)
 
-prediction = clf.predict(example_measures)
-print(prediction)
+# prediction = clf.predict(example_measures)
+# print(prediction)  
