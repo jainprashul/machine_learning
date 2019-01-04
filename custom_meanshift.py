66c212e3fd237c379e9fc0b7bdc0a5f04e78047b
Prashul Jain <jainprashul@gmail.com>
2019-01-04 16:17:11 +0530

    custom model mean_shift

--- custom_meanshift.py
+++ custom_meanshift.py
@@ -3,21 +3,21 @@ from matplotlib import style
 style.use('ggplot')
 import numpy as np
 
-X = np.array(([[1,2],
-               [1.5,1.8],
-               [5,8],
-               [8,8],
-               [1,0.6],
-               [9,11],
-               [1,3],
-               [8,2],
-               [10,2],
-               [9,3],
-                     [8,9],
-                     [0,3],
-                     [5,4],
-                     [7,8],
-                     [6,4],]))
+from sklearn.datasets.samples_generator import make_blobs
+
+X, y = make_blobs(n_samples=20, centers=3, n_features=2)
+
+# X = np.array(([[1,2],
+#                [1.5,1.8],
+#                [5,8],
+#                [8,8],
+#                [1,0.6],
+#                [9,11],
+#                [1,3],
+#                [8,2],
+#                [10,2],
+#                [9,3],
+#                      ]))
 
 # plt.scatter(X[:,0], X[:,1], s=150, linewidths=5)
 # plt.show()
@@ -53,31 +53,44 @@ class Mean_Shift:
             for i in centriods:
                 # num of points within radius / radius
                 in_bandwidth = []
-                centriod = centriods[i]
+                centroid = centriods[i]
 
                 for featureset in data:
-                    # if np.linalg.norm(featureset - centriod) < self.radius:
-                    #     in_bandwidth.append(featureset)
-                    distance = np.linalg.norm(featureset - centriod)
-                    if distance == 0 :
-                        distance = 0.00000001
-                    
-                    weights_index = int(distance/self.radius)
+                    #if np.linalg.norm(featureset-centroid) < self.radius:
+                    #    in_bandwidth.append(featureset)
+                    distance = np.linalg.norm(featureset- centroid)
+                    if distance == 0:
+                        distance = 0.00000000001
+                    weight_index = int(distance/self.radius)
                     if weight_index > self.radius_norm_step-1:
                         weight_index = self.radius_norm_step-1
 
-                    to_add = (weights[weight_index]**2)*[featureset]
+                    to_add = (weights[weight_index])*[featureset]
                     in_bandwidth +=to_add
 
-                    
-
-                
                 # generate new centriod location via mean avg
                 new_centriod = np.average(in_bandwidth, axis=0)
                 new_centriods.append(tuple(new_centriod))
             
             # this is for the coherent points removal 
             uniques = sorted(list(set(new_centriods)))
+
+            to_pop = []
+
+            for i in uniques:
+                for ii in [i for i in uniques]:
+                    if i == ii:
+                        pass
+                    elif np.linalg.norm(np.array(i)-np.array(ii)) <= self.radius:
+                        #print(np.array(i), np.array(ii))
+                        to_pop.append(ii)
+                        break
+
+            for i in to_pop:
+                try:
+                    uniques.remove(i)
+                except:
+                    pass
 
             prev_centroids = dict(centriods)
 
@@ -100,15 +113,37 @@ class Mean_Shift:
         
         self.centroids = centriods
 
+        self.classifications = {}
+
+        for i in range(len(self.centroids)):
+            self.classifications[i] = []
+            
+        for featureset in data:
+            #compare distance to either centroid
+            distances = [np.linalg.norm(featureset-self.centroids[centroid]) for centroid in self.centroids]
+            #print(distances)
+            classification = (distances.index(min(distances)))
+
+            # featureset that belongs to that cluster
+            self.classifications[classification].append(featureset)
+
     def predict(self, data):
-        pass
+        distances = [np.linalg.norm(data-self.centroids[centroid]) for centroid in self.centroids]
+            #print(distances)
+        classification = (distances.index(min(distances)))
+
 
 clf = Mean_Shift()
 clf.fit(X)
 
 centriods = clf.centroids
 
-plt.scatter(X[:,0], X[:,1], s=150)
+# plt.scatter(X[:,0], X[:,1], s=150)
+
+for classification in clf.classifications:
+    color = colors[classification]
+    for featureset in clf.classifications[classification]:
+        plt.scatter(featureset[0],featureset[1], marker='x', color=color, s=150, linewidths=4)
 
 for c in centriods:
     plt.scatter(centriods[c][0], centriods[c][1], color='k', marker='*', s=150)
